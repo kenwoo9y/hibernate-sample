7bc4307edf4bb91da5ac30b67492e001f21c5e01
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-17 00:01:03 +0900

    add methods of UserRepository Interface

--- src/main/java/com/example/hibernatesample/repository/UserRepository.java
+++ src/main/java/com/example/hibernatesample/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.example.hibernatesample.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hibernatesample.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
+
+    void changePassword(User user);
+
+    void deleteUser(String userId);
     
 }
