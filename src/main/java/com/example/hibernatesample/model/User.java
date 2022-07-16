9fdcaf09677d903989bc2c531ac0e48da1cf1c08
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-16 23:46:08 +0900

    update User class

--- src/main/java/com/example/hibernatesample/model/User.java
+++ src/main/java/com/example/hibernatesample/model/User.java
@@ -2,20 +2,45 @@ package com.example.hibernatesample.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users") //not user => because it is reserved.
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 80)
     private String userName;
+
+    @Column(name = "password", nullable = false, length = 80)
     private String password;
+
+    @Column(name = "email", nullable = false, length = 80)
     private String email;
+
+    @Column(name = "first_name", nullable=false, length=40)
     private String firstName;
+
+    @Column(name = "last_name", nullable=false, length=40)
     private String lastName;
+
+    @Column(name = "created_time", nullable = false)
     private LocalDateTime createdTime;
+
+    @Column(name = "updated_time", nullable = false)
     private LocalDateTime updatedTime;
 }
