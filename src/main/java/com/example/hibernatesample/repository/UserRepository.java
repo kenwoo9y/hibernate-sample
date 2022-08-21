3bc6ed372f30217cedb75537f88279338330cac9
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-21 16:27:26 +0900

    changed to update updated_time

--- src/main/java/com/example/hibernatesample/repository/UserRepository.java
+++ src/main/java/com/example/hibernatesample/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.hibernatesample.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +13,6 @@ import com.example.hibernatesample.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
-    @Query("update User set password = :password where id = :user_id")
-    void updatePassword(@Param("user_id") Long userId, @Param("password") String password);
+    @Query("update User set password = :password, updated_time = :updated_time where id = :user_id")
+    void updatePassword(@Param("user_id") Long userId, @Param("password") String password, @Param("updated_time") LocalDateTime updatedTime);
 }
